661328ee685c61e7e94c39c63ca151dd9e1069aa
VishalVyavahare <vishal.vyavahare@aeratechnology.com>
2020-10-03 16:45:30 +0530

    First Commit to GIT.

---
+++ src/test/java/com/example/BooksMgmt/BooksMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BooksMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
